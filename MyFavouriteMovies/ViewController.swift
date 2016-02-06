2cfd64fbcce3c5cc2521478e292b6181487207dd
Joshua Ide <Josh@192-168-1-100.tpgi.com.au>
2016-02-06 12:51:37 +1100

    Initial Commit

---
+++ MyFavouriteMovies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFavouriteMovies
+//
+//  Created by Joshua Ide on 6/02/2016.
+//  Copyright © 2016 Fox Gallery Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
