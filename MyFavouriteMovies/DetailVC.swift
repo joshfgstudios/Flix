6593fb347e1c4442e87c8c77686e1da9fc4ff6b4
Joshua Ide <Josh@192-168-1-100.tpgi.com.au>
2016-02-07 11:39:05 +1100

    Safe deleting

--- MyFavouriteMovies/DetailVC.swift
+++ MyFavouriteMovies/DetailVC.swift
@@ -32,12 +32,7 @@ class DetailVC: UIViewController {
         imgMovie.clipsToBounds = true
     }
     
-    //Actions
-    @IBAction func onBackPressed(sender: AnyObject) {
-        dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    @IBAction func onDeletePressed(sender: AnyObject) {
+    func deleteMovie(alert: UIAlertAction!) {
         let app = UIApplication.sharedApplication().delegate as! AppDelegate
         let context = app.managedObjectContext
         
@@ -50,6 +45,20 @@ class DetailVC: UIViewController {
             print("Could not delete item.")
         }
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    //Actions
+    @IBAction func onBackPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func onDeletePressed(sender: AnyObject) {
+        let alert = UIAlertController(title: "Delete?", message: "Deleting an entry is permanent and cannot be undone.  Are you sure?", preferredStyle: UIAlertControllerStyle.Alert)
+        let cancel = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+        //let delete = UIAlertAction(title: "Delete", style: UIAlertActionStyle.Destructive, handler: deleteMovie)
+        alert.addAction(cancel)
+        alert.addAction(UIAlertAction(title: "Delete", style: UIAlertActionStyle.Destructive, handler: deleteMovie))
+        presentViewController(alert, animated: true, completion: nil)
     }
     
 }
